97e4ab88334bb7af3a1202931d8fd38bfdf95b8a
Gatduel Riek Thiep <gatduelriek@gmail.com>
2026-08-25 14:50:45 +0300

    Add greet function and integrate into main program

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 # Let me import the functions from utuils.py
 from utuils import square, is_even, celsius_to_faherenheit
+
 number = int(input("Enter a number: "))
+name = input("Enter your name: ")
 print(f"The square of {number} is {square(number)}")
 print("Even" if is_even(number) else "Odd")
 print(f"{number} degree Celsius is equal to {celsius_to_faherenheit(number)} degree Fahrenheit")
